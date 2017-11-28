adc808d5c6097f318afd64f59400eee3ee0374af
Amaresh <amareshbingumalla@gmail.com>
2017-11-28 16:42:52 -0700

    Basic put and get functionalities Implemented

---
+++ myController.js
@@ -0,0 +1,25 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $http) {
+    $scope.artist = "";
+    $scope.songtitle = "";
+    $scope.tableData = "";
+    $scope.getData = function() {
+        //alert("HI " + $scope.firstName +" "+ $scope.lastName);
+        $scope.tableData = "";
+        $http.get("https://9tb9iunyvi.execute-api.us-east-2.amazonaws.com/dev/testresource")
+            .then(function(response) {
+                $scope.tableData = response.data;
+                console.log(response.data);
+            });
+    };
+    $scope.putData = function() {
+        $http.post(" https://9tb9iunyvi.execute-api.us-east-2.amazonaws.com/dev/insertmusic", {
+                artist: $scope.artist,
+                songtitle: $scope.songtitle
+            })
+            .then(function(response) {
+                $scope.insertedData = response.data;
+                console.log(response.data);
+            });
+    }
+});
